3faff35623320bbd995749f02fcdc458dd2ba6c4
LinQin <woshilinqin163@163.com>
2022-04-27 00:19:31 +0800

    feat: springboot 启动流程

--- src/main/java/com/GitbookApplication.java
+++ src/main/java/com/GitbookApplication.java
@@ -12,5 +12,11 @@ public class GitbookApplication {
         SpringApplication.run(GitbookApplication.class, args);
     }
 
+    /**
+     * 1. 准备环境，根据不同的环境创建不同的Environment
+     * 2. 准备、加载上下⽂，为不同的环境选择不同的Spring Context，然后加载资源，配置Bean
+     * 3. 初始化，这个阶段刷新Spring Context，启动应⽤
+     * 4. 最后结束流程
+     */
 
 }
